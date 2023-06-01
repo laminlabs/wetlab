da5a334558eb4d08e9921c746288f8eb3b1f7934
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-01 12:56:58 +0200

    ➖ Remove lnbase-biolab (#10)
    * :heavy_minus_sign: Remove lnbase-biolab
    
    * :green_heart: Fix

--- lnschema_lamin1/_core.py
+++ lnschema_lamin1/_core.py
@@ -1,18 +1,15 @@
+from datetime import datetime
 from typing import Optional
 
-from lnbase_biolab import (
-    BiosampleBase,
-    ExperimentBase,
-    ExperimentTypeBase,
-    TechsampleBase,
-    TreatmentBase,
-)
 from lnschema_bionty import CellLine, CellType, Disease, Species, Tissue
 from lnschema_core import File
+from lnschema_core._timestamps import CreatedAt, UpdatedAt
+from lnschema_core._users import CreatedBy
 from lnschema_core.dev.sqlmodel import add_relationship_keys, get_orm, schema_sqlmodel
 from sqlalchemy.orm import relationship
 from sqlmodel import Field, Relationship
 
+from lnschema_lamin1.dev import id as idg
 from lnschema_lamin1.dev import type as tp
 
 from . import __name__ as module_name
@@ -35,13 +32,27 @@ schema_sqlmodel(schema_name)
 SQLModel = get_orm(module_name)
 
 
-class ExperimentType(ExperimentTypeBase, table=True):  # type: ignore
+class ExperimentType(SQLModel, table=True):  # type: ignore
     """Experiment types."""
 
+    id: str = Field(default_factory=idg.experiment_type, primary_key=True)
+    name: str = Field(default=None, index=True)
+    efo_id: str = Field(default=None, unique=True)
+    created_by: str = CreatedBy
+    created_at: datetime = CreatedAt
+    updated_at: Optional[datetime] = UpdatedAt
 
-class Experiment(ExperimentBase, table=True):  # type: ignore
+
+class Experiment(SQLModel, table=True):  # type: ignore
     """Experiments."""
 
+    id: str = Field(default_factory=idg.experiment, primary_key=True)
+    name: str = Field(default=None, index=True)
+    date: datetime = Field(default=None, index=True)
+    """Date on which the experiment is performed."""
+    created_by: str = CreatedBy
+    created_at: datetime = CreatedAt
+    updated_at: Optional[datetime] = UpdatedAt
     experiment_type_id: str = Field(default=None, foreign_key=ExperimentType.id, index=True)
     files: File = Relationship(
         back_populates="experiments",
@@ -52,9 +63,14 @@ class Experiment(ExperimentBase, table=True):  # type: ignore
 File.experiments = relationship(Experiment, back_populates="files", secondary=FileExperiment.__table__)
 
 
-class Biosample(BiosampleBase, table=True):  # type: ignore
+class Biosample(SQLModel, table=True):  # type: ignore
     """Biological samples that are registered in experiments."""
 
+    id: str = Field(default_factory=idg.biosample, primary_key=True)
+    name: Optional[str] = Field(default=None, index=True)
+    created_by: str = CreatedBy
+    created_at: datetime = CreatedAt
+    updated_at: Optional[datetime] = UpdatedAt
     batch: Optional[str] = None
     species_id: Optional[str] = Field(default=None, foreign_key=Species.id, index=True)
     species: Species = Relationship()
@@ -88,7 +104,13 @@ class Well(SQLModel, table=True):  # type: ignore
 File.wells = relationship(Well, secondary=FileWell.__table__)
 
 
-class Treatment(TreatmentBase, table=True):  # type: ignore
+class Treatment(SQLModel, table=True):  # type: ignore
+    id: str = Field(default_factory=idg.treatment, primary_key=True)
+    name: Optional[str] = Field(default=None, index=True)
+    description: Optional[str] = Field(default=None, index=True)
+    created_by: str = CreatedBy
+    created_at: datetime = CreatedAt
+    updated_at: Optional[datetime] = UpdatedAt
     type: tp.treatment_type = Field(nullable=False, index=True)
     system: tp.treatment_system = Field(default=None, index=True)
     target: Optional[str] = Field(default=None, index=True)
@@ -109,7 +131,15 @@ File.cell_line = relationship(CellLine, secondary=FileCellLine.__table__)
 add_relationship_keys(File)
 
 
-class Techsample(TechsampleBase, table=True):  # type: ignore
+class Techsample(SQLModel, table=True):  # type: ignore
+    id: str = Field(default_factory=idg.techsample, primary_key=True)
+    name: Optional[str] = Field(default=None, index=True)
+    batch: Optional[str] = None
+    filepath_r1: Optional[str] = None
+    filepath_r2: Optional[str] = None
+    created_by: str = CreatedBy
+    created_at: datetime = CreatedAt
+    updated_at: Optional[datetime] = UpdatedAt
     biosamples: Biosample = Relationship(
         back_populates="techsamples",
         sa_relationship_kwargs=dict(secondary=BiosampleTechsample.__table__),
