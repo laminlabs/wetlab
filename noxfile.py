59b97d82f86e2ec0d2c69fc60344331bf47e4c44
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-11 13:55:12 +0200

    :green_heart: Upgrade

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import nox
 from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
 
@@ -14,6 +12,6 @@ def lint(session: nox.Session) -> None:
 @nox.session
 def build(session):
     login_testuser1(session)
-    session.install(".[dev,test]")
+    session.install(".[dev]")
     run_pytest(session)
     build_docs(session)
