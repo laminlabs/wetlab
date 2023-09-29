a3e5390811eed585dd879d4a34eb276a332b1440
Alex Wolf <f.alexander.wolf@gmail.com>
2023-09-29 10:33:31 +0200

    🎨 Add test and trigger (#34)

--- lnschema_lamin1/__init__.py
+++ lnschema_lamin1/__init__.py
@@ -25,6 +25,15 @@ __version__ = "0.23.1"
 
 from lamindb_setup import _check_instance_setup
 
+
+# trigger instance loading if users
+# want to access attributes
+def __getattr__(name):
+    if name not in {"models"}:
+        _check_instance_setup(from_lamindb=True)
+    return globals()[name]
+
+
 if _check_instance_setup():
     from .models import (  # noqa
         Biosample,
