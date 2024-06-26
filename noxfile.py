c9a599683f54462e167695ef303d21732e8c6f35
Alex Wolf <f.alexander.wolf@gmail.com>
2024-06-26 16:51:48 +0200

    ♻️ Simplify

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,5 @@
 import nox
-from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
+from laminci.nox import build_docs, run_pre_commit, run_pytest
 
 nox.options.default_venv_backend = "none"
 
@@ -16,7 +16,6 @@ def install(session: nox.Session) -> None:
 
 @nox.session
 def test(session: nox.Session) -> None:
-    login_testuser1(session)
     run_pytest(session)
 
 
