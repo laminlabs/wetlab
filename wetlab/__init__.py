d0f034d6e9d70e5bf0464a36875bf192480eb88d
Alex Wolf <f.alexander.wolf@gmail.com>
2024-09-29 21:29:54 +0200

    ⚡️ Lazy module reload

--- wetlab/__init__.py
+++ wetlab/__init__.py
@@ -1,15 +1,9 @@
-"""Registries for wetlab related entities.
+"""Registries for wetlab entities.
 
-Generally, the wetlab schema is designed to model experimental covariates and perturbations including their targets.
-
-This default wetlab schema module mostly serves to exemplify `how you define
-custom registries on GitHub <https://github.com/laminlabs/wetlab/blob/main/wetlab/models.py>`__.
-
-Most professional users create their custom schemas to map their operations or
-leverage schema synching from Benchling. If you need help with this, please `reach out <https://lamin.ai/contact>`__!
+See the `source code
+<https://github.com/laminlabs/wetlab/blob/main/wetlab/models.py>`__.
 
 >>> pip install wetlab
-
 >>> lamin init --storage ./test-wetlab --schema bionty,wetlab
 
 Import wetlab:
@@ -44,11 +38,9 @@ __version__ = "0.33.0"
 from lamindb_setup import _check_instance_setup
 
 
-# trigger instance loading if users
-# want to access attributes
 def __getattr__(name):
-    if name not in {"models"}:
-        _check_instance_setup(from_lamindb=True)
+    if name != "models":
+        _check_instance_setup(from_module="wetlab")
     return globals()[name]
 
 
