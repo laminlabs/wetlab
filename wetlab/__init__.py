51f25c41d968c407f477d4a5d891b04f0517a641
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-12-17 15:36:38 +0100

    ✨ PertCurator

--- wetlab/__init__.py
+++ wetlab/__init__.py
@@ -70,6 +70,7 @@ if _check_instance_setup():
     import lamindb
 
     del __getattr__  # delete so that imports work out
+    from ._pert_curator import PertCurator
     from .models import (
         Biologic,
         Biosample,
