1c4f5be29828fdc87cbf284bca05b7e2ccc71a33
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-12-19 14:59:24 +0100

    💚 Fix

--- noxfile.py
+++ noxfile.py
@@ -20,15 +20,11 @@ def lint(session: nox.Session) -> None:
 def install(session: nox.Session) -> None:
     run(session, f"uv pip install {SYSTEM} --upgrade scanpy")
     run(session, f"uv pip install {SYSTEM} .[dev]")
+    install_lamindb(session, branch="main", extras="bionty,jupyter,ourprojects")
     run(
         session,
         f"uv pip install {SYSTEM} --no-deps git+https://github.com/laminlabs/cellxgene-lamin",
     )
-    run(
-        session,
-        f"uv pip install {SYSTEM} git+https://github.com/laminlabs/ourprojects",
-    )
-    install_lamindb(session, branch="main", extras="bionty,jupyter,ourprojects")
 
 
 @nox.session
