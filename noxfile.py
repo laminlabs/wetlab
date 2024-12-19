3493bc55a7d8d752a691f386caea131f5b115d3f
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-12-19 14:24:10 +0100

    💚 Nox

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,12 @@
 import nox
-from laminci.nox import SYSTEM, build_docs, run, run_pre_commit, run_pytest
+from laminci.nox import (
+    SYSTEM,
+    build_docs,
+    install_lamindb,
+    run,
+    run_pre_commit,
+    run_pytest,
+)
 
 nox.options.default_venv_backend = "none"
 
@@ -11,28 +18,13 @@ def lint(session: nox.Session) -> None:
 
 @nox.session
 def install(session: nox.Session) -> None:
-    run(
-        session,
-        f"uv pip install {SYSTEM} git+https://github.com/laminlabs/lamindb-setup",
-    )
-    run(
-        session,
-        f"uv pip install {SYSTEM} git+https://github.com/laminlabs/lnschema-core",
-    )
-    run(session, f"uv pip install {SYSTEM} git+https://github.com/laminlabs/bionty")
-    run(
-        session, f"uv pip install {SYSTEM} git+https://github.com/laminlabs/ourprojects"
-    )
+    run(session, f"uv pip install {SYSTEM} --upgrade scanpy")
+    run(session, f"uv pip install {SYSTEM} .[dev]")
     run(
         session,
         f"uv pip install {SYSTEM} --no-deps git+https://github.com/laminlabs/cellxgene-lamin",
     )
-    run(
-        session,
-        f"uv pip install {SYSTEM} git+https://github.com/laminlabs/lamindb",
-    )
-    run(session, f"uv pip install {SYSTEM} .[dev]")
-    run(session, f"uv pip install {SYSTEM} scanpy")
+    install_lamindb(session, branch="main", extras="bionty,jupyter,ourprojects")
 
 
 @nox.session
