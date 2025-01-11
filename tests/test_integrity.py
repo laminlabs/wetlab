16b5434a5f8a3044bc6d14e7ef11b3060a2c537b
Alex Wolf <f.alexander.wolf@gmail.com>
2025-01-11 23:07:14 +0100

    🚚 Deprecate the --schema argument of lamin init in favor of --modules

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -4,7 +4,7 @@ import pytest
 
 @pytest.fixture(scope="module")
 def setup_instance():
-    ln_setup.init(storage="./testdb", schema="bionty,wetlab")
+    ln_setup.init(storage="./testdb", modules="bionty,wetlab")
     yield
     ln_setup.delete("testdb", force=True)
 
