d8131fe741aab7da87a2b78606a129d3c3f91d74
Lukas Heumos <lukas.heumos@posteo.net>
2025-01-16 16:33:09 +0100

    🎨 Upgrade Ruff & mypy
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- wetlab/__init__.py
+++ wetlab/__init__.py
@@ -71,25 +71,25 @@ def __getattr__(name):
 if _check_instance_setup():
     del __getattr__  # delete so that imports work out
     try:
-      from ._pert_curator import PertCurator
-      from .models import (
-         Biologic,
-         Biosample,
-         CombinationPerturbation,
-         Compound,
-         CompoundPerturbation,
-         Donor,
-         EnvironmentalPerturbation,
-         Experiment,
-         GeneticPerturbation,
-         PerturbationTarget,
-         Techsample,
-         Well,
-      )
+        from ._pert_curator import PertCurator
+        from .models import (
+            Biologic,
+            Biosample,
+            CombinationPerturbation,
+            Compound,
+            CompoundPerturbation,
+            Donor,
+            EnvironmentalPerturbation,
+            Experiment,
+            GeneticPerturbation,
+            PerturbationTarget,
+            Techsample,
+            Well,
+        )
     except RuntimeError as e:
-      if "isn't in an application in INSTALLED_APPS" in str(e):
-         raise RuntimeError(
-               "the 'wetlab' module is missing from this instance.\n"
-               "Please add it via the settings page on laminhub."
-         ) from e
-      raise
+        if "isn't in an application in INSTALLED_APPS" in str(e):
+            raise RuntimeError(
+                "the 'wetlab' module is missing from this instance.\n"
+                "Please add it via the settings page on laminhub."
+            ) from e
+        raise
