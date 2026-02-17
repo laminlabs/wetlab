efbf260082836c7d1bdaea435d9197889dc40810
Alex Wolf <f.alexander.wolf@gmail.com>
2026-02-18 00:11:39 +0100

    ♻️ Point to most recent squashed migration

--- pertdb/migrations/0002_biologic_created_on_and_more.py
+++ pertdb/migrations/0002_biologic_created_on_and_more.py
@@ -7,7 +7,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("lamindb", "0182_squashed"),
+        ("lamindb", "0183_squashed"),
         ("pertdb", "0001_initial"),
     ]
 
